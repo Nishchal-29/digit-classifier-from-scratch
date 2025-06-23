2bfcb9c5f6ac751db75d479c36995a0467a753a7
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-06-23 16:12:07 +0530

    loss with logits

--- loss/crossentropyLoss.py
+++ loss/crossentropyLoss.py
@@ -1,7 +1,17 @@
 import numpy as np
 
-def cross_entropy_loss(Y_hat, Y_true):
-    m = Y_true.shape[0]
-    log_likelihood = -np.log(Y_hat[Y_true, np.arrange(m)] + 1e-15)
-    loss = np.sum(log_likelihood) / m
-    return loss
+def cross_entropy(logits, labels):
+    shifted_logits = logits - np.max(logits, axis=0, keepdims=True)
+    exp_logits = np.exp(shifted_logits)
+    probs = exp_logits / np.sum(exp_logits, axis=0, keepdims=True)
+
+    N = labels.shape[0]
+    one_hot = np.zeros_like(probs.T)
+    one_hot[np.arange(N), labels] = 1
+    one_hot = one_hot.T
+
+    log_likelihood = -np.log(probs[labels, np.arange(N)] + 1e-9)
+    loss = np.sum(log_likelihood) / N
+    dlogits = (probs - one_hot) / N
+
+    return loss, dlogits
