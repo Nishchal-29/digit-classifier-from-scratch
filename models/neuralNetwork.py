fb4fcf5259eb1f1a1dc0917c87164a7d0c28202a
Nishchal Singh <nishchalsingh9a@gmail.com>
2025-06-23 16:12:38 +0530

    update nn

--- models/neuralNetwork.py
+++ models/neuralNetwork.py
@@ -1,5 +1,5 @@
 class NeuralNetwork:
-    def __inti__(self, layers):
+    def __init__(self, layers):
         self.layers = layers
     
     def forward(self, X):
@@ -8,23 +8,22 @@ class NeuralNetwork:
             A = layer.forward(A)
         return A
     
-    def backward(self, Y_hat, Y_true):
-        dA = self.layers[-1].backward(Y_hat, Y_true)
-        for layer in reversed(self.layers[:-1]):
+    def backward(self, dLoss):
+        dA = dLoss
+        for layer in reversed(self.layers):
             dA = layer.backward(dA)
-        return dA
-    
+
     def get_params_and_grads(self):
         params = {}
         grads = {}
-        cnt = 1
+        count = 1
         for layer in self.layers:
             if hasattr(layer, 'params'):
-                layer_params = layer.params()
-                layer_grads = layer.grads()
-                params[f"W{cnt}"] = layer_params['W']
-                params[f"b{cnt}"] = layer_params['b']
-                grads[f"dW{cnt}"] = layer_grads['dW']
-                grads[f"db{cnt}"] = layer_grads['db']
-                cnt += 1
-        return params, grads  
+                p = layer.params()
+                g = layer.grads()
+                params[f"W{count}"] = p["W"]
+                params[f"b{count}"] = p["b"]
+                grads[f"dW{count}"] = g["dW"]
+                grads[f"db{count}"] = g["db"]
+                count += 1
+        return params, grads
